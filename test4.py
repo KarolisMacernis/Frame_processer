c242ee9da17e1622f2f1483ec3aa1a883205c9ef
GainPython <gainpython@gmail.com>
2024-01-15 16:54:19 +0200

    Implement DPI change functionality in image processing app
    The commit refactors the image editing tool to now include a function to change the DPI of image files within a specified directory. Additional utility to rename the processed files according to set patterns was also implemented. Enhanced UI behavior to enable processing button once an image directory has been selected.

--- test4.py
+++ test4.py
@@ -1,20 +1,13 @@
 import sys
-
+import os
 from PyQt6 import QtCore, QtWidgets, QtGui
 from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout
 from PyQt6.QtWidgets import QFileDialog, QLabel, QSizePolicy, QFormLayout, QComboBox, QMainWindow
 import PyQt6.QtWidgets as qtw
-
-class EditImages:
-    def __init__(self, dpi):
-        self.dpi = dpi
-
-    def adjust_dpi(self):
-        print(self.dpi)
-
-img = EditImages(300)
+from PIL import Image
+import re
 
 class MyApp(QWidget):
 
@@ -101,32 +94,113 @@ class MyApp(QWidget):
         self.project_name.setDisabled(True)
         self.project_name.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
-        # Create a "Process" button and connect it with the functions used for renaming the files and creating the project
-        # zip file.
+        # Create a "Process" button and connect it with the functions used for renaming the files and creating the
+        # project zip file.
         self.btn_process = QPushButton('Process')
         self.btn_process.setMinimumSize(270, 47)
         self.layout.addWidget(self.btn_process)
-        # self.btn_process.setDisabled(True)
+        self.btn_process.setDisabled(True)
         self.btn_process.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        # self.btn_process.clicked.connect(self.get_dpi)
+        self.btn_process.clicked.connect(self.change_dpi)
 
-        self.btn_process.clicked.connect(img.adjust_dpi())
+        # self.btn_process.clicked.connect(self.img.adjust_dpi)
 
         self.form_layout.addRow(self.dpi_instructions, self.dpi_combo)
         self.form_layout.addRow(self.numbering_instructions, self.numbering_combo)
         self.form_layout.addRow(self.name_instructions, self.project_name)
 
-    # def get_dpi(self):
-    #     """Get the dpi value that was chosen by the user."""
-    #     dpi = (self.dpi_combo.currentText())
-    #     return dpi
+
+
     def search_dir(self):
         """Choose the directory of the project folder and enable the "Process" button once done."""
         self.folder_name = QFileDialog.getExistingDirectory()
-        self.output_label.setText(f"Selected path:   {self.folder_name}")  # Set the label to the chosen folder directory.
-        # self.btn_res.setDisabled(False)
-        print(self.rect().height())
-        print(self.btn_dir.font().pointSize())
+        self.output_label.setText(f"Selected path:   {self.folder_name}")  # Set the label to the chosen folder
+        # directory.
+
+
+
+    def change_dpi(self):
+
+
+        """Change the dpi value of the images."""
+        folder_path = self.folder_name
+
+        new_folder = os.path.join(folder_path, "Adjusted Images")
+
+        img_extensions = (".png", ".jpg", ".jpeg", ".tiff", ".bmp")
+
+        if not os.path.exists(new_folder):  # Create the directory if it doesn't exist
+            os.mkdir(new_folder)
+
+        dpi_chosen = self.dpi_combo.currentText()
+        dpi_numeric = int(''.join(c for c in dpi_chosen if c.isdigit()))  # extract the numeric dpi value.
+
+        name_extracted = self.project_name.text()
+
+        self.numbering_chosen = self.numbering_combo.currentText()
+        self.number_seperation = self.numbering_chosen[0]
+        self.first_digit = int(self.numbering_chosen[1])
+        file_counter = 1  # index of the image
+
+        if dpi_chosen == "Use original":  # Change the dpi of the images if the user chooses so.
+            for filename in os.listdir(folder_path):  # Loop through all files in the directory
+                if filename.lower().endswith(img_extensions):  # Check if the file is an image
+                    img = Image.open(os.path.join(folder_path, filename))  # Open the image file
+                    ext = os.path.splitext(filename)[1]
+                    if self.first_digit == 0:
+                        # Count the number of images
+                        file_count = len(
+                            [name for name in os.listdir(folder_path) if name.lower().endswith(img_extensions)])
+                        num_digits = len(str(file_count))  # Get the total digits required
+
+                        new_filepath = os.path.join(
+                            new_folder,
+                            f"{name_extracted}{self.number_seperation}{str(file_counter).zfill(num_digits)}{ext}"
+                        )
+                        img.save(new_filepath)
+                        file_counter += 1
+
+                    else:
+                        file_count = len(
+                            [name for name in os.listdir(folder_path) if name.lower().endswith(img_extensions)])
+                        num_digits = len(str(file_count))  # Get the total digits required
+
+                        new_filepath = os.path.join(
+                            new_folder,
+                            f"{name_extracted}{self.number_seperation}{str(file_counter)}{ext}"
+                        )
+                        img.save(new_filepath)
+                        file_counter += 1
+
+        else:
+            for filename in os.listdir(folder_path):  # Loop through all files in the directory
+                if filename.lower().endswith(img_extensions):  # Check if the file is an image
+                    img = Image.open(os.path.join(folder_path, filename))  # Open the image file
+                    ext = os.path.splitext(filename)[1]
+                    if self.first_digit == 0:
+                        # Count the number of images
+                        file_count = len(
+                            [name for name in os.listdir(folder_path) if name.lower().endswith(img_extensions)])
+                        num_digits = len(str(file_count))  # Get the total digits required
+
+                        new_filepath = os.path.join(
+                            new_folder,
+                            f"{name_extracted}{self.number_seperation}{str(file_counter).zfill(num_digits)}{ext}"
+                        )
+                        img.save(new_filepath, dpi=(dpi_numeric, dpi_numeric))
+                        file_counter += 1
+
+                    else:
+                        file_count = len(
+                            [name for name in os.listdir(folder_path) if name.lower().endswith(img_extensions)])
+                        num_digits = len(str(file_count))  # Get the total digits required
+
+                        new_filepath = os.path.join(
+                            new_folder,
+                            f"{name_extracted}{self.number_seperation}{str(file_counter)}{ext}"
+                        )
+                        img.save(new_filepath, dpi=(dpi_numeric, dpi_numeric))
+                        file_counter += 1
 
     def enable_options(self):
         """Enable the image processing options once the image directory has been selected."""
@@ -136,6 +210,7 @@ class MyApp(QWidget):
         self.numbering_instructions.setDisabled(False)
         self.numbering_combo.setDisabled(False)
         self.project_name.setDisabled(False)
+        self.btn_process.setDisabled(False)
 
     def enable_process(self):
         """Enable the Process button."""
